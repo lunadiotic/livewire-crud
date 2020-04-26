edfbcba7813c5e63621b7390feac8931d934a0a5
IDStack <idstack@outlook.co.id>
2020-04-26 10:57:49 +0700

    prepare project

---
+++ app/Http/Livewire/ContactUpdate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ContactUpdate extends Component
+{
+    public function render()
+    {
+        return view('livewire.contact.contact-update');
+    }
+}
