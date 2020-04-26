6e556c9bd4bf3dc8b7fe09074b9a9d39e1c42542
IDStack <idstack@outlook.co.id>
2020-04-26 12:07:34 +0700

    Show Contacts Data

--- app/Http/Livewire/ContactIndex.php
+++ app/Http/Livewire/ContactIndex.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Livewire;
 
+use App\Contact;
 use Livewire\Component;
 
 class ContactIndex extends Component
 {
+    public $data, $name, $phone, $selectedId;
+    public $updateStatus = false;
+
     public function render()
     {
+        $this->data = Contact::all();
         return view('livewire.contact.contact-index');
     }
 }
--- resources/views/livewire/contact/contact-index.blade.php
+++ resources/views/livewire/contact/contact-index.blade.php
@@ -1,3 +1,27 @@
 <div>
-    {{-- Do your work, then step back. --}}
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Phone</th>
+              <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $no = 0 ?>
+            @foreach ($data as $contact)
+            <?php $no++ ?>
+            <tr>
+                <th scope="row">{{ $no }}</th>
+                <td>{{ $contact->name }}</td>
+                <td>{{ $contact->phone }}</td>
+                <td>
+                    <button class="btn btn-sm btn-info text-white">Edit</button>
+                    <button class="btn btn-sm btn-danger">Delete</button>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
